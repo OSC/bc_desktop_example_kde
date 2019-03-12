0b3db452cfad1a270820f566959097620f123818
Morgan Rodgers <mrodgers@osc.edu>
2019-03-12 11:50:17 -0400

    Simplify directory creation and remove duplicate startkde invocation

--- template/script.sh
+++ template/script.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 set -x
-# Launch the RStudio Server
+
 echo "Starting up KDE."
 
-export KDEHOME="$TMPDIR/.kde4-$USER"
-export XDG_RUNTIME_DIR="$TMPDIR/.kde4-$USER"
-startkde startkde 2>&1
+# Namespace all the KDE temporary directories for this session
+DESKTOP_TEMP=$(mktemp -d)
+export TMPDIR="$DESKTOP_TEMP"
+export XDG_RUNTIME_DIR="$DESKTOP_TEMP"
+startkde 2>&1
 
 echo 'KDE has exited.'
