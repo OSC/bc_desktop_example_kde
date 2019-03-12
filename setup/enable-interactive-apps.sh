67cecf833f4e2922bcb44d624ad69af5974579d2
Morgan Rodgers <mrodgers@osc.edu>
2019-03-12 11:46:57 -0400

    Improve docs

--- setup/enable-interactive-apps.sh
+++ setup/enable-interactive-apps.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 set -e
-# This will enable interactive apps on ood-images-full
 
+# This will enable interactive apps on ood-images-full
 printf "\nrnode_uri: '/rnode'\n" >> /etc/ood/config/ood_portal.yml
 printf "\nnode_uri: '/node'\n" >> /etc/ood/config/ood_portal.yml
 
 # Control which hosts are available for interactive app running by setting an Apache PCRE
 printf "\nhost_regex: 'head'\n" >> /etc/ood/config/ood_portal.yml
 
+# Update the installed portal configuration and restart Apache
 /opt/ood/ood-portal-generator/sbin/update_ood_portal
 systemctl try-restart httpd24-httpd.service httpd24-htcacheclean.service
+
+echo 'Finished enabling interactive apps'
